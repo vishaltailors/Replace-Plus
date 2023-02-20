958a979c23ca4d1f507e73879d42d1bc7faff928
VishalTailor <vishaltailors22@gmail.com>
2023-02-21 00:01:10 +0530

    Initial commit

---
+++ extension.js
@@ -0,0 +1,51 @@
+const vscode = require('vscode');
+
+function hasActiveEditor() {
+	return vscode.window.activeTextEditor;
+}
+
+function hasSelections() {
+	return vscode.window.activeTextEditor.selections.length > 0;
+}
+
+function activate() {
+	console.log("Replace plus is now active!");
+	vscode.commands.registerCommand('replace-plus.replaceWith', async function () {
+		if(!hasActiveEditor()) {
+			return;
+		}
+		if(!hasSelections()) {
+			vscode.window.showErrorMessage("You must select something to replace.");
+			return;
+		}
+		const input = await vscode.window.showInputBox({
+			prompt: 'Provide a string separated by comma or array to replace with',
+		})
+		if(!input) {
+			return;
+		}
+		let inputArr = input.split(',');
+		if(input.charAt(0) == "[" && input.charAt(input.length - 1) == "]") {
+			try{
+				inputArr = JSON.parse(JSON.stringify(input));
+			}catch(error) {
+				vscode.window.showErrorMessage("Invalid input");
+				return;
+			}
+		}
+		const editor = vscode.window.activeTextEditor;	
+		editor.edit((editBuilder) => {
+			editor.selections.forEach((selection, i) => {
+				editBuilder.replace(selection, inputArr[i]);
+			})
+		})
+	});
+
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
